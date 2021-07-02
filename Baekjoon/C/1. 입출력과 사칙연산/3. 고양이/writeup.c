39d8ba1b9355c638a1497e6b9e5c306c40c3304e
Phantester K <67867993+Kinesys@users.noreply.github.com>
2021-07-02 20:07:52 +0900

    file_master

---
+++ Baekjoon/C/1. 입출력과 사칙연산/3. 고양이/writeup.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+
+    return 0;
+}
+
+// \를 출력하기 위해서는 \\로 출력을 해야한다. (역슬래시 두 번 사용)
