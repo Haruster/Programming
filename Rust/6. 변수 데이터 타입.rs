a7d327b0ecf141a8f443fb06b9322b4ef45934c0
Your Name <you@your-domain.com>
2021-08-04 03:04:24 +0900

    rust_2

---
+++ Rust/6. 변수 데이터 타입.rs
@@ -0,0 +1,14 @@
+fn main() {
+    
+    let x = 45; // i32
+    let y: i64 = 45; //i64
+
+    let z: u64 = 45; //u64
+
+    let f = 6.7; //f32
+
+    let j: f64 = 6.7; //f64
+
+    let b:bool = false;
+
+}
