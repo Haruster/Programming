62563c8787d5a720bf073a20ada8456d9ff141d5
Your Name <you@your-domain.com>
2021-08-04 10:25:42 +0900

    if-else

---
+++ Rust/7. if~else 문.rs
@@ -0,0 +1,16 @@
+fn main() {
+
+    let n = 20;
+
+    if n < 30 {
+        println!("n은 30보다 작습니다.");
+    }
+
+    else if n == 20 {
+        println!("n은 20입니다.");
+    }
+
+    else {
+        println!("n의 값은 10입니다.");
+    }
+}
---
+++ Rust/8. while문.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let mut n = 10;
+
+    while n <= 50 {
+        println!("n은 {}입니다.", n);
+
+        n += 1;
+    }
+}
