a6f9391a3d77d8d68be3e20469b4b133274a7303
Your Name <you@your-domain.com>
2021-08-05 13:52:11 +0900

    variable

---
+++ Swift/3. 변수(숫자 출력).swift
@@ -0,0 +1,16 @@
+import Foundation
+
+var one = 1;
+var two = 2;
+var three = 3;
+var four = 4;
+
+var data_1 = one + two;
+var data_2 = three - one;
+var data_3 = four * three;
+var data_4 = four / two;
+
+print(data_1)
+print(data_2)
+print(data_3)
+print(data_4)
