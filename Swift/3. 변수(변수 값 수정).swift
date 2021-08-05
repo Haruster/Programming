44168129b6f83c413f5e51b943e9a2b73ad8b7c2
Your Name <you@your-domain.com>
2021-08-05 14:40:06 +0900

    s2

---
+++ Swift/3. 변수(변수 값 수정).swift
@@ -0,0 +1,5 @@
+import Foundation
+
+var a = 5
+var b = 3
+var c = 
---
+++ Swift/3. 변수(숫자 출력 -2).swift
@@ -0,0 +1,11 @@
+import Foundation
+
+var a = 1
+var b = 2
+var c = 3
+var d = 4
+
+print(a + b)
+print(d - c)
+print(c * d)
+print(d / b)
