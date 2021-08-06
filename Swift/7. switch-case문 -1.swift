9fd2239d1c985d946bc4ac3453baa40aa23244cf
Your Name <you@your-domain.com>
2021-08-07 04:58:42 +0900

    switch

---
+++ Swift/7. switch-case문 -1.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+var str = "a"
+
+switch str {
+    case "a": // case "변수값"
+        print("데이터는 A입니다.")
+    
+    case "b":
+        print("데이터는 B입니다.")
+    
+    default:
+        print("맞는 데이터 값이 없습니다.")
+}
+
+//switch-case문 : 변수값과 동일한 case문을 출력한다.
+
