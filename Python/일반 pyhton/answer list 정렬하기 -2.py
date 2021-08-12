f4779341d98ee4399c56c3db908b4dd4ff78557c
Your Name <you@your-domain.com>
2021-08-13 02:46:16 +0900

    python

---
+++ Python/일반 pyhton/answer list 정렬하기 -2.py
@@ -0,0 +1,6 @@
+my_list = ['apple', 'orange', 'banana', 'lemon']
+
+my_list.sort() # 정렬
+
+print(my_list) # ['apple', 'banana', 'lemon', 'orange']가 출력됨
+
---
+++ Python/일반 pyhton/answer 오름차순 정렬.py
@@ -0,0 +1,5 @@
+my_list = [5, 4, 3, 2, 1]
+
+my_list.sort()
+
+print(my_list) #[1, 2, 3, 4, 5]가 출력됨
