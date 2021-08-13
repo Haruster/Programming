df669feedb32f475288b5c412324870c311f8173
Your Name <you@your-domain.com>
2021-08-14 04:08:56 +0900

    시퀀스

---
+++ Python/일반 pyhton/answer 시퀀스 -2.py
@@ -0,0 +1,8 @@
+my_str = "Impossible"
+my_list = ["Apple", "Banana", "Chamwae", "Durian"]
+
+var1 = len(my_str) # my_str의 길이를 확인해서 변수 var1에 넣는다.
+var2 = "egg" in my_list # "egg"가 변수 my_list에 있는지 확인하고 있으면 true를 없으먄 false를 변수 var2에 넣는다.
+
+print(var1) #10아 출력됨.
+print(var2) #False가 출력됨.
---
+++ Python/일반 pyhton/answer 인덱싱, 슬라이싱.py
@@ -0,0 +1,8 @@
+my_str = "Impossible"
+my_list = ["Apple", "Banana", "Chamwae", "Durian"]
+
+var1 = my_list[2] #두번째 원소인 Chamwae를 var1에 넣는다.
+var2 = my_str[2:] #2자리부터 출력한다. (possible)
+
+print(var1) # Chamwae가 출력됨.
+print(var2) # possible가 출력됨.
