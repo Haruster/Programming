2a82d07c093db78bcf468ae3a59afd4e7d8fa286
Your Name <you@your-domain.com>
2021-08-22 02:38:57 +0900

    pyfunction

---
+++ Python/일반 pyhton(앨리스)/answer 합 길이 쉽게 구하기.py
@@ -0,0 +1,9 @@
+my_list = [1, 3, 6, 5, 2] #숫자 5개가 담긴 변수 my_list를 선언한다.
+
+var1 = sum(my_list) 
+var2 = len(my_list) # 변수 my_list의 길이를 구한다.
+var3 = var1 / var2 #변수 my_list의 평균을 구한다 (평균 = 합 / 길이)
+
+print(var1)
+print(var2)
+print(var3)
